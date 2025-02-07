44bd84b88cfb40359582702fa46186d7eb385d91
Gabriel Lobo <globo.coding@gmail.com>
2025-02-07 16:10:31 -0600

    Removed Localhost instance

--- src/app/(students-site)/community/page.tsx
+++ src/app/(students-site)/community/page.tsx
@@ -5,7 +5,7 @@ import JobsSection from "../../../components/Community/sections/JobsSection";
 import BlogSection from "../../../components/Community/sections/BlogSection";
 
 async function fetchBlogs (): Promise<BlogResponse> {
-    const res = await fetch(process.env.SERVER_API + "api/blogs", { cache: "no-store" });
+    const res = await fetch(process.env.NEXT_PUBLIC_PAYLOAD_URL + "api/blogs", { cache: "no-store" });
     if (!res.ok) throw new Error("Failed to fetch blog posts");
     return res.json();
 }
